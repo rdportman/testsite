2ce9070d577f02b4fa035c6016e5cca11c59018e
rdportman <rdp2104@columbia.edu>
2017-04-29 08:18:08 -0400

    deleted stuff

--- docs/shiny_cancer-data/www/message-handler.js
+++
@@ -1,8 +0,0 @@
-// This file listens for messages from the shiny app and
-// redirects them to javascript
-
-Shiny.addCustomMessageHandler("equalizePlotHeight",
-  function(message) {
-    equalizePlotHeight(message.target, message.by);
-  }
-);
